0fdc366a58ab7ec0483b7fb1ef70f3340a9c9604
Steven Zhang <zhzhxtrrk@gmail.com>
2015-09-14 09:43:44 +0800

    config transport via cmdline args

--- js/client.js
+++ js/client.js
@@ -1,15 +1,18 @@
 var t = require('./tunnel');
+var transport = t.Transport.TCP;
 var serverHost = '127.0.0.1';
 var serverPort = 9000;
-if (process.argv.length == 4) {
+if (process.argv.length >= 4) {
     serverHost = process.argv[2];
     serverPort = parseInt(process.argv[3]);
+    if (process.argv.length >= 5 && process.argv[4] == 'HTTP')
+        transport = t.Transport.HTTP;
 }
 // start client
 t.connect({
     serverHost: serverHost,
     serverPort: serverPort,
-    transport: t.Transport.HTTP,
+    transport: transport,
     accessKey: 'helloworld',
     password: 'anythingwhichisusedtoencryptthepackets'
 }).listen(9001);
